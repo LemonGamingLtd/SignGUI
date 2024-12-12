84e749a7fa28807766a247d7cbe6fea20526fce0
Nahuel Dolores <contact@nahu.me>
2024-12-12 12:46:15 +0100

    Mark as Folia after merge

--- api/src/main/java/de/rapha149/signgui/SignGUIAction.java
+++ api/src/main/java/de/rapha149/signgui/SignGUIAction.java
@@ -1,6 +1,6 @@
 package de.rapha149.signgui;
 
-import org.apache.commons.lang.Validate;
+import org.apache.commons.lang3.Validate;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
